bffc794070983f850340a35bc887b2e57e05061d
student <albisharafiova@yandex.ru>
2024-11-28 15:51:11 +0300

    s

--- app/src/main/java/ru/example/myapplication/MainActivity.kt
+++ app/src/main/java/ru/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.example.myapplication
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -10,8 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding:ActivityMainBinding  = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
-        val kittyArray:ArrayList<Kitty> =
+        val kittyArray = arrayListOf<Kitty>(
+            Kitty("коричневый", "Иммануил", "Сиамский"),
+            Kitty("белый", "Барсик", "Британский"),
+            Kitty("черный", "Феликс", "Вислоухий"),
+            Kitty("коричневый", "Брэд", "Сфинкс"),
+            Kitty("серый", "Роза", "Мейн-Кун"),
+            Kitty("белый", "Фиона", "Бурма"),
+            Kitty("черный", "Шрек", "Американская"),
+            Kitty("коричневый", "Филадельфия", "Шотландская")
+            )
+        val adapter = KittyAdapter(kittyArray)
+        binding.RV.adapter = adapter
+        binding.lifecycleOwner = this
 
-
+    }
+    fun fromDrawabletoBitMap():Bitmap{
+        reco
     }
 }
